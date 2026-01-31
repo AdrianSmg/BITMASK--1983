f32afac0231b6a623039351747d1e2477bc9b3dd
Adrián San Miguel <adriansmg06@gmail.com>
2026-01-31 01:37:05 +0100

    Write

--- Scripts/typewritterAnim.gd
+++ Scripts/typewritterAnim.gd
@@ -2,37 +2,45 @@ extends Control
 
 @export var typing_speed: float = 0.03
 
-@onready var label: Label = $Label
+@onready var output_scroll: ScrollContainer = %OutputScroll
+@onready var output: RichTextLabel = %Output
 
 var _full_text := ""
 var _timer := 0.0
 var _typing := false
 
-func set_text(text: String):
-	_full_text = text
-	label.text = text
-	label.visible_characters = 0
+func _update_output_size_and_scroll() -> void:
+	# Espera a que el RichTextLabel recalcule el contenido
+	await get_tree().process_frame
 
-func set_text_color(color: Color):
-	label.add_theme_color_override("font_color", color)
+	# Fuerza a que el hijo "mida" lo que ocupa su contenido
+	# (esto hace que el ScrollContainer tenga overflow real)
+	output.custom_minimum_size.y = output.get_content_height()
 
-func set_font(font: Font):
-	label.add_theme_font_override("font", font)
+	# Espera a que el ScrollContainer actualice su scrollbar
+	await get_tree().process_frame
+	output_scroll.scroll_vertical = output_scroll.get_v_scroll_bar().max_value
 
-func start_typing():
+func type_append(text: String) -> void:
+	_full_text = output.text + text
+	output.text = _full_text
+
+	# revela SOLO lo nuevo
+	output.visible_characters = _full_text.length() - text.length()
+
 	_timer = 0.0
 	_typing = true
-	label.visible_characters = 0
+	_update_output_size_and_scroll()
 
-func _process(delta):
+func _process(delta: float) -> void:
 	if not _typing:
 		return
 
 	_timer += delta
-
 	if _timer >= typing_speed:
 		_timer = 0.0
-		label.visible_characters += 1
+		output.visible_characters += 1
+		_update_output_size_and_scroll()
 
-		if label.visible_characters >= _full_text.length():
+		if output.visible_characters >= _full_text.length():
 			_typing = false
