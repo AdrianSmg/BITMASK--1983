bef8b9c6d41507e9927009b59f3d2cf8dcefa697
AsheN-007 <david.s.zugarramurdi@gmail.com>
2026-01-31 01:44:34 +0000

    vuelta a la normalidad

--- Scripts/input.gd
+++ Scripts/input.gd
@@ -3,22 +3,10 @@ extends Control
 @onready var input: LineEdit = $Input
 var text_scene = preload("res://Scenes/text.tscn")
 var font = preload("res://Fonts/osc_mono.ttf")
+
 func _ready():
 	input.grab_focus()
 	
-func show_dialogue(pos, text, _font):
-	var bubble = text_scene.instantiate()
-	
-	add_child(bubble)
-
-	bubble.position = pos
-
-	bubble.set_text(text)
-	bubble.set_text_color(Color.GREEN)
-	bubble.set_font(_font)
-	bubble.start_typing()
-
-
 func _on_input_text_submitted(text: String) -> void:
 	if text.is_empty():
 		return
@@ -30,3 +18,15 @@ func _on_input_text_submitted(text: String) -> void:
 		
 	input.clear()
 	input.grab_focus()
+
+func show_dialogue(pos, text, _font):
+	var bubble = text_scene.instantiate()
+	
+	add_child(bubble)
+
+	bubble.position = pos
+
+	bubble.set_text(text)
+	bubble.set_text_color(Color.GREEN)
+	bubble.set_font(_font)
+	bubble.start_typing()
--- Scripts/typewritterAnim.gd
+++ Scripts/typewritterAnim.gd
@@ -2,44 +2,37 @@ extends Control
 
 @export var typing_speed: float = 0.03
 
-@onready var output_scroll: ScrollContainer = %OutputScroll
-@onready var output: RichTextLabel = %Output
+@onready var label: Label = $Label
 
 var _full_text := ""
 var _timer := 0.0
 var _typing := false
 
-func _update_output_size_and_scroll() -> void:
-	# Espera a que el RichTextLabel recalcule el contenido
-	await get_tree().process_frame
+func set_text(text: String):
+	_full_text = text
+	label.text = text
+	label.visible_characters = 0
 
-	# Fuerza a que el hijo "mida" lo que ocupa su contenido
-	# (esto hace que el ScrollContainer tenga overflow real)
-	output.custom_minimum_size.y = output.get_content_height()
+func set_text_color(color: Color):
+	label.add_theme_color_override("font_color", color)
 
-	# Espera a que el ScrollContainer actualice su scrollbar
-	await get_tree().process_frame
-	output_scroll.scroll_vertical = output_scroll.get_v_scroll_bar().max_value
+func set_font(font: Font):
+	label.add_theme_font_override("font", font)
 
-func type_append(text: String) -> void:
-	_full_text = output.text + text
-	output.text = _full_text
-
-	output.visible_characters = _full_text.length() - text.length()
-
+func start_typing():
 	_timer = 0.0
 	_typing = true
-	_update_output_size_and_scroll()
+	label.visible_characters = 0
 
-func _process(delta: float) -> void:
+func _process(delta):
 	if not _typing:
 		return
 
 	_timer += delta
+
 	if _timer >= typing_speed:
 		_timer = 0.0
-		output.visible_characters += 1
-		_update_output_size_and_scroll()
+		label.visible_characters += 1
 
-		if output.visible_characters >= _full_text.length():
+		if label.visible_characters >= _full_text.length():
 			_typing = false
